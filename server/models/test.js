f9bdfec513d84cb5a026b77465aac0bc52b290ee
Shawn <814517196@qq.com>
2018-01-19 15:29:55 +0800

    commit

--- server/models/test.js
+++ server/models/test.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 //var dao = require('../server/db/dao.js');
 /* GET home page. */
-router.get('/test1', function(req, res, next) {
+router.get('/.well-known/pki-validation/fileauth.txt', function(req, res, next) {
     //function getInfo(error, recordInfo, affected){
     //  //console.log("数据行数" + recordInfo.recordsets.length);
     //  //if(recordInfo.recordsets.length > 1){
@@ -11,7 +11,7 @@ router.get('/test1', function(req, res, next) {
     //  res.json(recordInfo);
     //}
     //dao.query('SELECT TOP 3* FROM URDailyorders WHERE cmonth LIKE  \'%2017%\'', getInfo);
-    res.json({info:'测试信息1'});
+    res.send({info:'测试信息1'});
 });
 
 router.get('/test2', function(req, res, next) {
